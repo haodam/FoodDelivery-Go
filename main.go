c0d85ebca63055227e07dd9a4258e6dda9421094
dam hao <damanhhao3004@gmail.com>
2022-10-31 17:52:21 +0700

    feat: connect db

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"log"
+)
 
 func main() {
 	fmt.Println("FoodDelivery-Go")
+	dsn := "root:@tcp(127.0.0.1:3306)/?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println(db, err)
 }

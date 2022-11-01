41263f50f48bf4915e3a7e87961f57b7252275a8
haodam <damanhhao3004.com>
2022-11-02 01:12:21 +0700

    update fix connect db

--- main.go
+++ main.go
@@ -1,21 +1,23 @@
 package main
 
 import (
+	"log"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
-	"log"
 )
 
 type Restaurant struct {
 	Id   int    `json:"id" gorm:"column:id;"`
 	Name string `json:"name" gorm:"column:name;"`
 	Addr string `json:"addr" gorm:"column:addr;"`
+	//Phone string `json:"phone" gorm:"column:phone;"`
 }
 
 func (Restaurant) TableName() string { return "restaurants" }
 
 func main() {
-	dsn := "root:@tcp(127.0.0.1:3306)/?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:@tcp(127.0.0.1:3306)/fooddelivery?charset=utf8mb4&parseTime=True&loc=Local"
 	//dsn := os.Getenv("MYSQL_CONN_STRING")
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
@@ -24,8 +26,9 @@ func main() {
 	}
 
 	//log.Println(db, err)
+	db.AutoMigrate(&Restaurant{})
 
 	newRestaurant := Restaurant{Name: "Trau Ngon Quan", Addr: "So 1 Nguyen Van Cu"}
 
-	db.Create(&newRestaurant)
+	db.Table("restaurants").Create(&newRestaurant)
 }

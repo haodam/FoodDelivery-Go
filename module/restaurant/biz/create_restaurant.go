1cfc02a05e89f9a2b5576b87eab4873e6a309e0e
dam hao <damanhhao3004@gmail.com>
2022-11-07 19:44:07 +0700

    feat: Clean Architecture , add business interface create_restaurant

---
+++ module/restaurant/biz/create_restaurant.go
@@ -0,0 +1,29 @@
+package restaurantbiz
+
+import (
+	restaurantmodel "FoodDelivery/module/restaurant/model"
+	"context"
+	"errors"
+)
+
+type CreateRestaurantStore interface {
+	CreateRestaurant(context context.Context, data *restaurantmodel.RestaurantCreate) error
+}
+
+type createRestaurantBiz struct {
+	store CreateRestaurantStore
+}
+
+func NewCreateRestaurantBiz(store CreateRestaurantStore) *createRestaurantBiz {
+	return &createRestaurantBiz{store: store}
+}
+
+func (biz *createRestaurantBiz) CreateRestaurant(context context.Context, data *restaurantmodel.RestaurantCreate) error {
+	if data.Name == "" {
+		return errors.New("name cannot be empty")
+	}
+	if err := biz.store.CreateRestaurant(context, data); err != nil {
+		return err
+	}
+	return nil
+}

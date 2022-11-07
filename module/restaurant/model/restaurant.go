7aefdc17e035a5914930724062cc9f8ee9041d6c
dam hao <damanhhao3004@gmail.com>
2022-11-07 19:45:04 +0700

    feat: add model struct restaurant

---
+++ module/restaurant/model/restaurant.go
@@ -0,0 +1,24 @@
+package restaurantmodel
+
+type Restaurant struct {
+	Id   int    `json:"id" gorm:"column:id;"`
+	Name string `json:"name" gorm:"column:name;"`
+	Addr string `json:"addr" gorm:"column:addr;"`
+}
+
+func (Restaurant) TableName() string { return "restaurants" }
+
+type RestaurantCreate struct {
+	Id   int    `json:"id" gorm:"column:id;"` // Tag
+	Name string `json:"name" gorm:"column:name;"`
+	Addr string `json:"addr" gorm:"column:addr;"`
+}
+
+func (RestaurantCreate) TableName() string { return Restaurant{}.TableName() }
+
+type RestaurantUpdate struct {
+	Name *string `json:"name" gorm:"column:name;"`
+	Addr *string `json:"addr" gorm:"column:addr;"`
+}
+
+func (RestaurantUpdate) TableName() string { return Restaurant{}.TableName() }

87e1eab59162d01903825c01699837b0719b0028
dam hao <damanhhao3004@gmail.com>
2022-11-08 18:41:38 +0700

    feat: add feature delete food delivery API

---
+++ module/restaurant/storage/delete.go
@@ -0,0 +1,15 @@
+package restaurantstorage
+
+import (
+	restaurantmodel "FoodDelivery/module/restaurant/model"
+	"context"
+)
+
+func (s *sqlStore) Delete(context context.Context, id int) error {
+	if err := s.db.Table(restaurantmodel.Restaurant{}.TableName()).
+		Where("id = ?", id).Updates(map[string]interface{}{"status": 0}).Error; err != nil {
+		return err
+	}
+	return nil
+
+}

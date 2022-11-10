8f9e4bf04f62ca63ae142e75d7670d6cab8ed3de
dam hao <damanhhao3004@gmail.com>
2022-11-10 19:08:19 +0700

    feat: create function for client seen to database

---
+++ module/restaurant/transport/ginrestaurant/list_restaurant.go
@@ -0,0 +1,48 @@
+package ginrestaurant
+
+import (
+	"FoodDelivery/common"
+	"FoodDelivery/component/appctx"
+	restaurantbiz "FoodDelivery/module/restaurant/biz"
+	restaurantmodel "FoodDelivery/module/restaurant/model"
+	restaurantstorage "FoodDelivery/module/restaurant/storage"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func ListRestaurant(appCtx appctx.AppContext) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		db := appCtx.GetMaiDBConnection()
+
+		var pagingData common.Paging
+		if err := c.ShouldBind(&pagingData); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		pagingData.Fulfill()
+
+		var filter restaurantmodel.Filter
+		if err := c.ShouldBind(&filter); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		//var result []restaurantmodel.Restaurant
+		store := restaurantstorage.NewSQLStore(db)
+		biz := restaurantbiz.NewListRestaurantBiz(store)
+
+		result, err := biz.ListRestaurant(c.Request.Context(), &filter, &pagingData)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		c.JSON(http.StatusOK, common.NewSuccessResponse(result, pagingData, filter))
+	}
+}

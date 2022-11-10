7b697280dbd4707fdc2d00eec5bf6416de665429
dam hao <damanhhao3004@gmail.com>
2022-11-10 19:00:44 +0700

    feat: add function list restaurant for API

---
+++ module/restaurant/biz/list_restaurant.go
@@ -0,0 +1,31 @@
+package restaurantbiz
+
+import (
+	"FoodDelivery/common"
+	restaurantmodel "FoodDelivery/module/restaurant/model"
+	"context"
+)
+
+type ListRestaurantStore interface {
+	ListDataWithCondition(ctx context.Context, filter *restaurantmodel.Filter, paging *common.Paging,
+		moreKeys ...string) ([]restaurantmodel.Restaurant, error)
+}
+
+type listRestaurantBiz struct {
+	store ListRestaurantStore
+}
+
+func NewListRestaurantBiz(store ListRestaurantStore) *listRestaurantBiz {
+	return &listRestaurantBiz{store: store}
+}
+
+func (biz *listRestaurantBiz) ListRestaurant(ctx context.Context, filter *restaurantmodel.Filter, paging *common.Paging,
+	moreKeys ...string) ([]restaurantmodel.Restaurant, error) {
+
+	result, err := biz.store.ListDataWithCondition(ctx, filter, paging)
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}

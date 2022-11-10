2072c5f4581805ec0492aa1ee8824894a3123261
dam hao <damanhhao3004@gmail.com>
2022-11-10 19:03:56 +0700

    feat: create floor storage create list restaurant

---
+++ module/restaurant/storage/list.go
@@ -0,0 +1,31 @@
+package restaurantstorage
+
+import (
+	"FoodDelivery/common"
+	restaurantmodel "FoodDelivery/module/restaurant/model"
+	"context"
+)
+
+func (s *sqlStore) ListDataWithCondition(ctx context.Context, filter *restaurantmodel.Filter, paging *common.Paging,
+	moreKeys ...string) ([]restaurantmodel.Restaurant, error) {
+
+	var result []restaurantmodel.Restaurant
+	db := s.db.Table(restaurantmodel.Restaurant{}.TableName()).Where("status in (1)")
+
+	if f := filter; f != nil {
+		if f.OwnerId > 0 {
+			db = db.Where("owner_id = ?", f.OwnerId)
+		}
+	}
+
+	if err := db.Count(&paging.Total).Error; err != nil {
+		return nil, err
+	}
+
+	offset := (paging.Page - 1) * paging.Limit
+	if err := db.Offset(offset).Limit(paging.Limit).Order("id desc").Find(&result).Error; err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}

3d437ea07a2b73c5de51e25ba567069102427e0a
haodam <damanhhao3004.com>
2022-11-14 00:00:28 +0700

    update: handdler err

--- module/restaurant/transport/ginrestaurant/create_restaurant.go
+++ module/restaurant/transport/ginrestaurant/create_restaurant.go
@@ -12,22 +12,21 @@ import (
 
 func CreateRestaurant(appCtx appctx.AppContext) gin.HandlerFunc {
 	return func(c *gin.Context) {
+
 		db := appCtx.GetMaiDBConnection()
+
 		var data restaurantmodel.RestaurantCreate
 
 		if err := c.ShouldBind(&data); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{
-				"error": err.Error(),
-			})
+			c.JSON(http.StatusBadRequest, common.ErrInvalidRequest(err))
 			return
 		}
 		//db.Create(&data)
 		store := restaurantstorage.NewSQLStore(db)
 		biz := restaurantbiz.NewCreateRestaurantBiz(store)
+
 		if err := biz.CreateRestaurant(c.Request.Context(), &data); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{
-				"error": err.Error(),
-			})
+			c.JSON(http.StatusBadRequest, err)
 			return
 		}
 

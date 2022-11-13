ef82787d1f7b1ae77152ed0f80ad93dfa6f459a6
haodam <damanhhao3004.com>
2022-11-13 23:59:54 +0700

    update 13-11

--- module/restaurant/storage/list.go
+++ module/restaurant/storage/list.go
@@ -10,11 +10,14 @@ func (s *sqlStore) ListDataWithCondition(ctx context.Context, filter *restaurant
 	moreKeys ...string) ([]restaurantmodel.Restaurant, error) {
 
 	var result []restaurantmodel.Restaurant
-	db := s.db.Table(restaurantmodel.Restaurant{}.TableName()).Where("status in (1)")
+	db := s.db.Table(restaurantmodel.Restaurant{}.TableName())
 
 	if f := filter; f != nil {
 		if f.OwnerId > 0 {
 			db = db.Where("owner_id = ?", f.OwnerId)
+		}
+		if len(f.Status) > 0 {
+			db = db.Where("status in (?)", f.Status)
 		}
 	}
 

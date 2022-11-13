8c22e814fd0d73cff451779b35448c5f39ee0e4c
haodam <damanhhao3004.com>
2022-11-13 23:46:08 +0700

    feat: more features response err

---
+++ common/sql_model.go
@@ -0,0 +1,10 @@
+package common
+
+import "time"
+
+type SQLModel struct {
+	Id        int        `json:"id" gorm:"column:id;"`
+	Status    int        `json:"status" gorm:"column:status;default:1;"`
+	CreatedAt *time.Time `json:"created_at,omitempty" gorm:"created_at"`
+	UpdatedAt *time.Time `json:"updated_at,omitempty" gorm:"updated_at"`
+}

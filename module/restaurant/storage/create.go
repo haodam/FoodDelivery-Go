9c1eae9905547a93931cc4c18873fa59701da56e
haodam <damanhhao3004.com>
2022-11-13 23:59:12 +0700

    update: handdler err

--- module/restaurant/storage/create.go
+++ module/restaurant/storage/create.go
@@ -1,13 +1,14 @@
 package restaurantstorage
 
 import (
+	"FoodDelivery/common"
 	restaurantmodel "FoodDelivery/module/restaurant/model"
 	"context"
 )
 
 func (s *sqlStore) Create(ctx context.Context, data *restaurantmodel.RestaurantCreate) error {
 	if err := s.db.Create(&data).Error; err != nil {
-		return err
+		return common.ErrDB(err)
 	}
 	return nil
 }

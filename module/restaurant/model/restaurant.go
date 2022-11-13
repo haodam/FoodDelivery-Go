e60c1f3f28055f0d7cb59cd776a0baa48c20182d
haodam <damanhhao3004.com>
2022-11-13 23:58:25 +0700

    feat: add validate and err

--- module/restaurant/model/restaurant.go
+++ module/restaurant/model/restaurant.go
@@ -1,18 +1,41 @@
 package restaurantmodel
 
+import (
+	"FoodDelivery/common"
+	"errors"
+	"strings"
+)
+
+type RestaurantType string
+
+const (
+	TypeNormal  RestaurantType = "normal"
+	TypePremium RestaurantType = "premium"
+	EntityName                 = "Restaurant"
+)
+
 type Restaurant struct {
-	Id     int    `json:"id" gorm:"column:id;"`
-	Name   string `json:"name" gorm:"column:name;"`
-	Addr   string `json:"addr" gorm:"column:addr;"`
-	Status int    `json:"status" gorm:"column:status;"`
+	common.SQLModel
+	Name string         `json:"name" gorm:"column:name;"`
+	Addr string         `json:"addr" gorm:"column:addr;"`
+	Type RestaurantType `json:"type" gorm:"column:type;"`
 }
 
 func (Restaurant) TableName() string { return "restaurants" }
 
 type RestaurantCreate struct {
-	Id   int    `json:"id" gorm:"column:id;"` // Tag
+	common.SQLModel
 	Name string `json:"name" gorm:"column:name;"`
 	Addr string `json:"addr" gorm:"column:addr;"`
+}
+
+func (data *RestaurantCreate) Validate() error {
+	data.Name = strings.TrimSpace(data.Name)
+
+	if data.Name == "" {
+		return ErrNameIsEmpty
+	}
+	return nil
 }
 
 func (RestaurantCreate) TableName() string { return Restaurant{}.TableName() }
@@ -23,3 +46,7 @@ type RestaurantUpdate struct {
 }
 
 func (RestaurantUpdate) TableName() string { return Restaurant{}.TableName() }
+
+var (
+	ErrNameIsEmpty = errors.New("name can not be empty")
+)

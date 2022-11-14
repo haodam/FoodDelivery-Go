13a0c5efe87e918a52f73cc5a0d748139215762d
dam hao <damanhhao3004@gmail.com>
2022-11-14 17:45:13 +0700

    feat: add middleware for API

---
+++ middleware/recover.go
@@ -0,0 +1,30 @@
+package middleware
+
+import (
+	"FoodDelivery/common"
+	"FoodDelivery/component/appctx"
+	"github.com/gin-gonic/gin"
+)
+
+func Recover(ac appctx.AppContext) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		defer func() {
+			if err := recover(); err != nil {
+				c.Header("content-Type", "application/json")
+
+				if appErr, ok := err.(*common.AppError); ok {
+					c.AbortWithStatusJSON(appErr.StatusCode, appErr)
+					panic(err)
+					return
+				}
+
+				appErr := common.ErrInternal(err.(error))
+				c.AbortWithStatusJSON(appErr.StatusCode, appErr)
+				panic(err)
+				return
+			}
+		}()
+
+		c.Next()
+	}
+}

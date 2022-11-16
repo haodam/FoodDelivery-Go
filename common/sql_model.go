21cde9e60680564c5e277d97ecdd88c935e583d4
haodam <damanhhao3004.com>
2022-11-17 01:36:24 +0700

    feat: add one struct FakeID

--- common/sql_model.go
+++ common/sql_model.go
@@ -3,8 +3,14 @@ package common
 import "time"
 
 type SQLModel struct {
-	Id        int        `json:"id" gorm:"column:id;"`
+	Id        int        `json:"-" gorm:"column:id;"`
+	FakeID    *UID       `json:"id" gorm:"-"`
 	Status    int        `json:"status" gorm:"column:status;default:1;"`
 	CreatedAt *time.Time `json:"created_at,omitempty" gorm:"created_at"`
 	UpdatedAt *time.Time `json:"updated_at,omitempty" gorm:"updated_at"`
+}
+
+func (m *SQLModel) GenUID(dbType int) {
+	uid := newUID(uint32(m.Id), dbType, 1)
+	m.FakeID = &uid
 }

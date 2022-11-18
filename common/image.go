55fdddcfe103ff4c7189ec52350ce38aaa1b3d9a
dam hao <damanhhao3004@gmail.com>
2022-11-18 18:38:45 +0700

    feat: up load image for API

---
+++ common/image.go
@@ -0,0 +1,62 @@
+package common
+
+import (
+	"database/sql/driver"
+	"encoding/json"
+	"errors"
+	"fmt"
+)
+
+type Image struct {
+	Id        int    `json:"id" gorm:"column: id"`
+	Url       string `json:"url" gorm:"column: url"`
+	Width     int    `json:"width" gorm:"column: width"`
+	Height    int    `json:"height" gorm:"column: height"`
+	CloudName string `json:"cloud_name,omitempty" gorm:"-"`
+	Extension string `json:"extension,omitempty" gorm:"-"`
+}
+
+func (Image) TableName() string { return "image" }
+
+// Go from DB --> client
+func (j *Image) Scan(value interface{}) error {
+	bytes, ok := value.([]byte)
+	if !ok {
+		return errors.New(fmt.Sprint("Failed to unmarshal"))
+	}
+	var img Image
+	if err := json.Unmarshal(bytes, &img); err != nil {
+		return err
+	}
+	*j = img
+	return nil
+}
+
+func (j *Image) Value() (driver.Value, error) {
+	if j == nil {
+		return nil, nil
+	}
+	return json.Marshal(j)
+}
+
+type Images []Image
+
+func (j *Images) Scan(value interface{}) error {
+	bytes, ok := value.([]byte)
+	if !ok {
+		return errors.New(fmt.Sprint("Failed to unmarshal"))
+	}
+	var img Images
+	if err := json.Unmarshal(bytes, &img); err != nil {
+		return err
+	}
+	*j = img
+	return nil
+}
+
+func (j *Images) Value() (driver.Value, error) {
+	if j == nil {
+		return nil, nil
+	}
+	return json.Marshal(j)
+}
